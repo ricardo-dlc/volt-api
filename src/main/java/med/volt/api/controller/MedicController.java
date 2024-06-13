e22734f9faa905fd8de3e1483fc4b44d6f1601a3
Ricardo de la Cruz <the_phantom_racer@hotmail.com>
2024-06-12 20:54:24 -0500

    Implement pagination

--- src/main/java/med/volt/api/controller/MedicController.java
+++ src/main/java/med/volt/api/controller/MedicController.java
@@ -3,6 +3,7 @@ package med.volt.api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,7 +28,7 @@ public class MedicController {
 	}
 
 	@GetMapping
-	public Page<MedicGetDTO> getMedics(Pageable page) {
+	public Page<MedicGetDTO> getMedics(@PageableDefault(size = 5) Pageable page) {
 		return medicRepository.findAll(page).map(MedicGetDTO::new);
 	}
 }
